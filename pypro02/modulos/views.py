8538d2be2c85927c630b085d3496f244fbc160ae
Fabio Zanetti <fczanetti@hotmail.com>
2023-12-20 16:58:33 -0300

    botao de login e logout criados

--- pypro02/modulos/views.py
+++ pypro02/modulos/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from pypro02.modulos import facade
+from django.contrib.auth.decorators import login_required
 
 
 def detalhe(request, slug):
@@ -8,6 +9,7 @@ def detalhe(request, slug):
     return render(request, 'modulos/modulo_detalhe.html', context={'modulo': modulo, 'aulas': aulas})
 
 
+@login_required
 def aula(request, slug):
     aula = facade.encontrar_aula(slug)
     return render(request, 'modulos/aula_detalhe.html', context={'aula': aula})
